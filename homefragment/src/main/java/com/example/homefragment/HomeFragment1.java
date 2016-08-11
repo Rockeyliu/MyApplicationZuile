e1927d4ac96b24ee817a74bf32bfc386c9a48f57
Rockey <294611503@qq.com>
2016-08-11 17:49:08 +0800

    Initial commit  og

---
+++ homefragment/src/main/java/com/example/homefragment/HomeFragment1.java
@@ -0,0 +1,13 @@
+package com.example.homefragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeFragment1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_fragment1);
+    }
+}
---
+++ minefragment/src/main/java/com/example/minefragment/MineFragment.java
@@ -0,0 +1,13 @@
+package com.example.minefragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MineFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine_fragment);
+    }
+}
